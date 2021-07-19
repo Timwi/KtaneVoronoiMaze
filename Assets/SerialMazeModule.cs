d79bde522a0887ac04f1edcf5a1eed6ba5a58359
Timwi <github@timwi.de>
2021-07-19 14:48:04 +0200

    Initial commit

---
+++ Assets/SerialMazeModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KModkit;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+
+/// <summary>
+/// On the Subject of Serial Maze
+/// Created by Timwi
+/// </summary>
+public class SerialMazeModule : MonoBehaviour
+{
+    public KMBombInfo Bomb;
+    public KMBombModule Module;
+    public KMAudio Audio;
+
+    private static int _moduleIdCounter = 1;
+    private int _moduleId;
+
+    void Start()
+    {
+        _moduleId = _moduleIdCounter++;
+    }
+}
